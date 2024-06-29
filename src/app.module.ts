fb880dee221716aa93b8dd119076c46c746e453d
StarayYaga <yagistdotcom@gmail.com>
2024-06-29 10:43:58 +0400

    Разметка проекта

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, DatabaseModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
